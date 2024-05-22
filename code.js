4996d9332135f152bfa8d3c95c8ab5ecf5bea8d8
Vadn7 <96883715+Vadn7@users.noreply.github.com>
2024-05-22 19:46:21 +0100

    We can add a text and there isn't a button left after the input

--- code.js
+++ code.js
@@ -1,25 +1,46 @@
-let box=document.getElementById("box")
-let text=document.getElementById("t1")
-let tp=text.value
-console.log(text)
 
-if(box.checked)
-{
-text.style.color="red"}
+/**
+ * 
+ *
+ insert la donnée dans un champ
+ Recupère la donnée
+ * 
+ */
+
+
+let box=document.getElementById('box')
+
+let para = document.createElement('p')
+
 
 function save()
 
 {
-    let tp=text.value
 
-    console.log(tp)
+    let text = document.getElementById('t1')   
+    let textValue = text.value
+    let check = document.getElementById('check')
+    let btn1 = document.getElementById('b1')
+
+    console.log(textValue)
 
 text.remove()
 
-let para=createElement("p")
-para.innerText=tp
 
-para.appendParent(body)
+
+para.innerText=textValue
+
+//oldParent.appendChild(wrapper);
+
+check.innerText=textValue;
+
+btn1.remove()
+
+}
+
+if(box.checked===true)
+    {
+    para.style.color="red"
 
 }
 
